e3a924d1f34ede18efc186258cffc99b0ad0208f
Roshang1704 <99422419+Roshang1704@users.noreply.github.com>
2022-02-11 17:28:01 +0530

    By Roshan

--- Demo GitHub/src/com/cjc/GitHubWithAbhilash.java
+++ Demo GitHub/src/com/cjc/GitHubWithAbhilash.java
@@ -5,12 +5,17 @@ package com.cjc;
  * @author - ROYAL_G
  *
  */
-public class GitHubWithAbhilash {
-
-
-	public static void main(String[] args) {
+public class GitHubWithAbhilash
+{
+	public void roshan()
+	{
+		System.out.println("demo");
+	}
+	public static void main(String[] args)
+	{
 
 		System.out.println("Hello Abhi, This is just a trial");
+		GitHubWithAbhilash d=new GitHubWithAbhilash();
+		d.roshan();
 	}
-
 }
