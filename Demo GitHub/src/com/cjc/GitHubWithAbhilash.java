cb467891c52a2e191ba166baabd2bfc75f39d3e9
USER <USER@ROYAL-G>
2022-02-11 17:00:49 +0530

    From Royal

---
+++ Demo GitHub/src/com/cjc/GitHubWithAbhilash.java
@@ -0,0 +1,16 @@
+
+package com.cjc;
+
+/*
+ * @author - ROYAL_G
+ *
+ */
+public class GitHubWithAbhilash {
+
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello Abhi, This is just a trial");
+	}
+
+}
